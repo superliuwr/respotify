fceb3d5c387f41b613175c5b4cf6d4b4220c8209
Marvin Liu <marvin.liu@fairfaxmedia.com.au>
2016-09-19 22:12:37 +1000

    Finish album list

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,11 +6,11 @@ class SearchBar extends React.Component {
     this.state = {
       searchTerm: ''
     };
-    this.hanldeInputChange = this.hanldeInputChange.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
   }
 
-  hanldeInputChange(event) {
+  handleInputChange(event) {
     this.setState({
       searchTerm: event.target.value
     });
@@ -23,12 +23,32 @@ class SearchBar extends React.Component {
   }
 
   render() {
-    return <input onChange={this.hanldeInputChange} onKeyPress={this.handleKeyPress} />;
+    return (
+      <div style={SearchBar.styles.div}>
+        <h3>Search for an Artist</h3>
+        <input
+          onChange={this.handleInputChange}
+          onKeyPress={this.handleKeyPress}
+          style={SearchBar.styles.input}
+        />
+      </div>
+    );
   }
 }
 
 SearchBar.propTypes = {
   getAlbums: React.PropTypes.func.isRequired
 };
+
+SearchBar.styles = {
+  div: {
+    margin: 30,
+    textAlign: 'center',
+  },
+  input: {
+    width: '60%',
+  },
+};
+
 
 export default SearchBar;
